16812380f8b2a0e640ad1d0c750c66ca7e34acda
Tao Lin <taoroalin@gmail.com>
2021-01-19 02:16:10 -0500

    fast very basic lisp parser

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,1 @@
-const exampleLisp =
-  "(hello (thing and thing (and thing) andy) ando)(hello (thing and thing (and thing) andy) ando)(hello (thing and thing (and thing) andy) ando)(hello (thing and thing (and thing) andy) ando)(hello (thing and thing (and thing) andy) ando)";
 
-const regexNotParen = /[^\(\)]+/;
-console.log("hoi()".match(regexNotParen));
-
-const stime = process.hrtime()[1];
-console.log(stime);
-let result = 0;
-for (let i = 0; i < 10000; i++) {
-  result += exampleLisp.substring(1, 200).length;
-}
-console.log((process.hrtime()[1] - stime) / 1000000);
